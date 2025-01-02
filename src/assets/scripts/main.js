9013ea113937cd9eb795eb3a01bfc4cec72ae2f0
Dafa Alvin <dafaalvin10@gmail.com>
2025-01-02 14:20:55 +0700

    style: added dark mode appearance

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,6 +1,38 @@
-const menuIcon = document.getElementById('menu-icon');
-const menuList = document.getElementById('menu-list');
+// Dark Mode
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const body = document.body;
 
-menuIcon.addEventListener('click', () => {
-    menuList.classList.toggle('hidden')
-})
+const darkMode = localStorage.getItem("darkMode");
+
+const enableDarkMode = () => {
+  body.classList.add("dark-mode");
+  darkModeToggle.innerHTML = '<i class="ph ph-moon"></i>';
+  localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+  body.classList.remove("dark-mode");
+  darkModeToggle.innerHTML = '<i class="ph ph-sun"></i>';
+  localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+  const darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+
+// Responsive Menu Navbar
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+});
